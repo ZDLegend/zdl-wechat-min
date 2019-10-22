a0b12cf2f5eb785fa62650ac56db176029a2afbf
张明浩 <463764941@qq.com>
2019-10-22 22:22:04 +0800

    修改显示内容

--- pages/images/my-data/my-data.js
+++ pages/images/my-data/my-data.js
@@ -9,17 +9,14 @@ let tolkData = {
   articleList: [{
       id: 1,
       level: 1,
-      title: "寒战",
-      avatar: "/image/avatar/avatar-04.jpg",
-      name: "游子女°",
-      thumbnail: "/image/thumbnail/thumbnail04.jpg",
-      content: "《寒战》是银都机构有限公司、万诱引力丁有限公司、安乐影片有限公司" +
-      "联合出品的动作电影，由梁乐民、陆剑青执导，郭富城、梁家辉、杨采妮领衔主演。该" +
-      "片讲述了在新时代背景下，一辆价值不菲的警察冲锋车被劫持，随之整个香港都陷入到" +
-      "安全危机，警匪之间展开高智商较量。",
+      title: "晴天",
+      avatar: "/image/avatar/head.jpeg",
+      name: "ZDLegend",
+    thumbnail: "https://7a64-zdlegend-o1ov1-1300454709.tcb.qcloud.la/images/time/2015.jpeg?sign=1f6f595cc4c87e5f18b325779aa88652&t=1571752098",
+      content: "刮风这天，我试过握着你手",
       views: 637,
       likes: 436,
-      date: "2018/03/12 14:28:38",
+      date: "2015/12/17 22:28:38",
       canLike: true,
       music: {
           dataUrl: "http://up.mcyt.net/?down/37626.mp3",
@@ -29,16 +26,14 @@ let tolkData = {
     }, {
       id: 2,
       level: 2,
-      title: "神偷奶爸3",
-      avatar: "/image/avatar/avatar-05.jpg",
-      name: "笑如歌",
-      thumbnail: "/image/thumbnail/thumbnail05.jpg",
-      content: "《神偷奶爸3》（Despicable Me 3）是由环球影业及照明娱乐公司联合制作，" +
-      "凯尔·巴尔达、皮艾尔·柯芬、埃里克·吉隆联合执导，史蒂夫·卡瑞尔、克里斯汀·韦格、崔·帕克" +
-      "等主要配音的3D动画电影。",
+      title: "星晴",
+      avatar: "/image/avatar/head.jpeg",
+      name: "ZDLegend",
+      thumbnail: "https://7a64-zdlegend-o1ov1-1300454709.tcb.qcloud.la/images/time/2016.jpeg?sign=16b0a3bbd54eba07fe2bcf608c03d151&t=1571752150",
+      content: "手牵手一步两步三步四步望着天",
       views: 862,
       likes: 639,
-      date: "2018/03/12 14:28:38",
+      date: "2016/12/17 22:28:388",
       canLike: true,
       music: {
           dataUrl: "http://up.mcyt.net/?down/46645.mp3",
@@ -48,17 +43,14 @@ let tolkData = {
     }, {
       id: 3,
       level: 3,
-      title: "如何摄影",
-      avatar: "/image/avatar/avatar-01.jpg",
-      name: "倾国倾城倾人心",
-      thumbnail: "/image/thumbnail/thumbnail01.jpg",
-      content: "运用摄影机、镜头、胶片把客观事物及其运动记录下来的过程。" +
-      "电影摄影基于视觉暂留原理，通过摄影机内部的间歇机构将连续的运动分解为" +
-      "一系列静止画面，再通过放映机的运转将这一系列静止影像连续呈现出来，从" +
-      "而再现运动的幻觉。",
+      title: "简单爱",
+      avatar: "/image/avatar/head.jpeg",
+      name: "欧也妮葛朗台·张",
+      thumbnail: "https://7a64-zdlegend-o1ov1-1300454709.tcb.qcloud.la/images/time/2017.jpeg?sign=2f221dc56fb9f0cb2285b34e7f8ed9a5&t=1571752186",
+      content: "我想就这样牵着你的手不放开",
       views: 228,
       likes: 68,
-      date: "2018/03/12 14:28:38",
+      date: "2017/12/17 22:28:38",
       canLike: true,
       music: {
           dataUrl: "http://up.mcyt.net/?down/46635.mp3",
@@ -68,15 +60,14 @@ let tolkData = {
     }, {
       id: 4,
       level: 4,
-      title: "开国大典",
-      avatar: "/image/avatar/avatar-02.jpg",
-      name: "安静苦笑",
-      thumbnail: "/image/thumbnail/thumbnail02.jpg",
-      content: "开国大典，是指1949年10月1日15：00在北京为中华人民共和国中央" +
-      "人民政府成立而举行的仪式，直到21：00才结束，是中华人民共和国成立的标志。",
+      title: "甜甜的",
+      avatar: "/image/avatar/head.jpeg",
+      name: "张黯浩",
+      thumbnail: "https://7a64-zdlegend-o1ov1-1300454709.tcb.qcloud.la/images/time/2018.jpeg?sign=a4244617d4b45adefb87d5e1ba08b410&t=1571752217",
+      content: "我尝着你话里面的奶油溜啊溜，听过的每句话都很可口哟啊哟",
       views: 173,
       likes: 126,
-      date: "2018/03/12 14:28:38",
+      date: "2018/12/17 22:28:38",
       canLike: true,
       music: {
           dataUrl: "http://up.mcyt.net/?down/46634.mp3",
@@ -86,50 +77,21 @@ let tolkData = {
     }, {
       id: 5,
       level: 5,
-      title: "冰雪奇缘",
-      avatar: "/image/avatar/avatar-03.jpg",
-      name: "十寒九冬薄荷心已凉",
-      thumbnail: "/image/thumbnail/thumbnail03.jpg",
-      content: "《冰雪奇缘》（Frozen）是2013年迪士尼出品3D的动画电影，由克里" +
-      "斯·巴克、珍妮弗·李执导，克里斯汀·贝尔、伊迪娜·门泽尔等参与主要配音。" +
-      "该片于2013年11月27日在美国上映。该片改编自安徒生童话《白雪皇后》，讲述小" +
-      "国阿伦黛尔因一个魔咒永远地被冰天雪地覆盖，为了寻回夏天，安娜公主和山民克里" +
-      "斯托夫以及他的驯鹿搭档组队出发，展开一段拯救王国的历险。2014年，该片获得第" +
-      "86届美国奥斯卡金像奖最佳动画长片。斯托夫以及他的驯鹿搭档组队出发，展开一段拯" +
-      "86届美国奥斯卡金像奖最佳动画长片。斯托夫以及他的驯鹿搭档组队出发，展开一段拯" +
-      "86届美国奥斯卡金像奖最佳动画长片。斯托夫以及他的驯鹿搭档组队出发，展开一段拯" +
-      "86届美国奥斯卡金像奖最佳动画长片。斯托夫以及他的驯鹿搭档组队出发，展开一段拯" +
-       "86届美国奥斯卡金像奖最佳动画长片。",
+      title: "七里香",
+      avatar: "/image/avatar/head.jpeg",
+      name: "张黯浩",
+      thumbnail: "https://7a64-zdlegend-o1ov1-1300454709.tcb.qcloud.la/images/time/2019.jpeg?sign=40976c36adfbb41972e65bdbef9c9f38&t=1571752238",
+      content: "把永远爱你写进诗的结尾",
       views: 822,
       likes: 352,
-      date: "2018/03/12 14:28:38",
+      date: "2019/12/17 22:28:38",
       canLike: true,
       music: {
           dataUrl: "http://up.mcyt.net/?down/46631.mp3",
           title: "九张机-锦零",
           coverImgUrl: "http://oeff2vktt.bkt.clouddn.com/image/66.jpg"
       }
-    }, {
-      id: 6,
-      level: 6,
-      title: "摄影的艺术",
-      avatar: "/image/avatar/avatar-06.jpg",
-      name: "柚苏°",
-      thumbnail: "/image/thumbnail/thumbnail06.jpg",
-      content: "摄影一词是源于希腊语φῶς phos（光线）和 γραφι graphis（绘画、绘图）" +
-      "或γραφή graphê，两字一起的意思是”以光线绘图”。摄影是指使用某种专门设备进行影像" +
-      "记录的过程，一般我们使用机械照相机或者数码照相机进行摄影。有时摄影也会被称为照相，也就是" +
-      "通过物体所反射的光线使感光介质曝光的过程。有人说过的一句精辟的语言：摄影家的能力是把日常" +
-      "生活中稍纵即逝的平凡事物转化为不朽的视觉图像。",
-      views: 732,
-      likes: 103,
-      date: "2018/03/12 14:28:38",
-      canLike: true,
-      music: {
-          dataUrl: "http://oeff2vktt.bkt.clouddn.com/image/7.jpg",
-          title: "豆花之歌-锦零",
-          coverImgUrl: "http://up.mcyt.net/?down/46632.mp3"
-      }
-  }]
+    }
+  ]
 }
 module.exports = tolkData;
