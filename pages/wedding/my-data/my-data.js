1e27eec8d750eba615eb88414dbb2d942a4c0696
张明浩 <463764941@qq.com>
2019-11-27 10:03:15 +0800

    添加婚纱照照片

--- pages/wedding/my-data/my-data.js
+++ pages/wedding/my-data/my-data.js
@@ -54,20 +54,62 @@ let imageData = {
   "wedding":[
     {
       "_id": "7d016cea-d5b4-4dc4-bf08-6e70fe8ba05c",
-      "coverageUrl": "https://pic2.zhimg.com/80/v2-1a924775e501cdb22db503813bdcf111_hd.jpg",
-      "explain": "测试图片",
+      "coverageUrl": "https://pic4.zhimg.com/v2-cb2e304a8ac9b82125828a7a9e5e5713_r.jpg",
+      "explain": "这是内景最正式的一套，有很多，就放一张好了",
       "type": 3
     },
     {
       "_id": "7d016cea-d5b4-4dc4-bf08-6e70fe8ba05c",
-      "coverageUrl": "https://pic4.zhimg.com/80/v2-1f36cc3e3907115de3d3ad2b45e7b1e3_hd.jpg",
-      "explain": "测试图片2",
+      "coverageUrl": "https://pic3.zhimg.com/v2-e2cb6c7eac96162ce98fa72cea64a5d2_r.jpg",
+      "explain": "内景古装，摄影师说俏皮一点",
       "type": 3
     },
     {
       "_id": "7d016cea-d5b4-4dc4-bf08-6e70fe8ba05c",
-      "coverageUrl": "https://pic3.zhimg.com/80/v2-62ba45a7e9c13bae6988ebe5711f7106_hd.jpg",
-      "explain": "测试图片3",
+      "coverageUrl": "https://pic3.zhimg.com/v2-12611826cdd71847bc0fa1faa130010e_r.jpg",
+      "explain": "内景古装，正儿八经坐在那好奇怪",
+      "type": 3
+    },
+    {
+      "_id": "7d016cea-d5b4-4dc4-bf08-6e70fe8ba05c",
+      "coverageUrl": "https://pic4.zhimg.com/v2-c3b07ad6bb90d0108caaae445763e88b_r.jpg",
+      "explain": "森系估计是所有新婚夫妻拍婚纱照必备吧",
+      "type": 3
+    },
+    {
+      "_id": "7d016cea-d5b4-4dc4-bf08-6e70fe8ba05c",
+      "coverageUrl": "https://pic4.zhimg.com/v2-c87660ff87d79a435661270c413f7353_r.jpg",
+      "explain": "这个走在草坪上的照片感觉更加自然，我挺喜欢的",
+      "type": 3
+    },
+    {
+      "_id": "7d016cea-d5b4-4dc4-bf08-6e70fe8ba05c",
+      "coverageUrl": "https://pic4.zhimg.com/v2-b29a27f4fd74acedce473e2eeb3a0abf_r.jpg",
+      "explain": "在母校建行楼走廊，我们毕业于用一所大学，算是一种小幸运吧",
+      "type": 3
+    },
+    {
+      "_id": "7d016cea-d5b4-4dc4-bf08-6e70fe8ba05c",
+      "coverageUrl": "https://pic2.zhimg.com/v2-849baa04bdd5f5e89211249f025668d9_r.jpg",
+      "explain": "从操场到教室，路过在维修的图书馆，没能拍出好看的照片好可惜",
+      "type": 3
+    },
+    {
+      "_id": "7d016cea-d5b4-4dc4-bf08-6e70fe8ba05c",
+      "coverageUrl": "https://pic3.zhimg.com/v2-ca852b5884fc6cdee28ce87103e68c6e_r.jpg",
+      "explain": "这一刻，仿佛回到了大学时代，坐在教室里就犯困",
+      "type": 3
+    },
+    {
+      "_id": "7d016cea-d5b4-4dc4-bf08-6e70fe8ba05c",
+      "coverageUrl": "https://pic2.zhimg.com/v2-2eb6e88994bd8d978ad3687da24fe66d_r.jpg",
+      "explain": "这是我最喜欢的两张，因为当时雨后的天空太绚烂了",
+      "type": 3
+    },
+    {
+      "_id": "7d016cea-d5b4-4dc4-bf08-6e70fe8ba05c",
+      "coverageUrl": "https://pic3.zhimg.com/v2-ba86c751827809e36aab23f9e8306df6_r.jpg",
+      "explain": "最后天都黑了，累到昏厥",
       "type": 3
     }
   ]
